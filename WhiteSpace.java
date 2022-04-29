c97fa543292b87edcd766155b8e1cd0178640a0c
Rahul-KP <rahulkp01@gmail.com>
2022-04-29 21:57:54 +0530

    Made Stack Manupilation Class

---
+++ WhiteSpace.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+class Stack_manipulation {
+	public Stack<Integer> s;
+	public Stack_manipulation() {
+		s = new Stack<Integer>();
+	}
+	public void push(int num) {
+		s.push(num);
+	}
+	public void duplicateTop() {
+		s.push((Integer)s.peek());
+	}
+	public void copy(int index) {
+		s.push((Integer)s.get(index));
+	}
+	public void swap() {
+		// Integer t = s.get(s.size()-2);//returns the 2nd element from top
+		Integer t1 = (Integer)s.pop();
+		Integer t2 = (Integer)s.pop();
+		s.push(t1);
+		s.push(t2);
+	}
+	public void discard() {
+		s.pop();
+	}
+	public void slide(int n) {
+		Integer top = (Integer)s.pop();
+		for(int i=0;i<n;i++) 
+			this.discard();
+		s.push(top);
+	}
+}
+public class WhiteSpace {
+	public static void main(String[] args) {
+		Stack_manipulation st = new Stack_manipulation();
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);					//1,2,3,4,5 -top->5
+		System.out.println(st.s);
+		
+		st.duplicateTop();			//1,2,3,4,5,5
+		System.out.println(st.s);
+		
+		st.copy(1);					//1,2,3,4,5,5,2
+		System.out.println(st.s);
+		
+		st.swap();					//1,2,3,4,5,2,5
+		System.out.println(st.s);
+	}
+}
