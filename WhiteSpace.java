9e244f85c13533ffdc67bc3d2696d4859a7b25e3
Rahul-KP <rahulkp01@gmail.com>
2022-05-16 22:22:13 +0530

    Fixed indendation

--- WhiteSpace.java
+++ WhiteSpace.java
@@ -13,23 +13,23 @@ public class WhiteSpace {
 			int r = 0;
 			while((r = fis.read()) != -1) {
 				// System.out.print((char)r);
-				if(r== 32){ 
+				if(r== 32) { 
 					System.out.println("Stack Manipulation");
 				}
 
-				else if(r==9){
+				else if(r==9) {
 					r=fis.read();
-					if(r == 32){
+					if(r == 32) {
 						System.out.println("Arithmetic");
 					} 
-					else if(r == 9){
+					else if(r == 9) {
 						System.out.println("Heap Access");
 					} 
-					else if(r == 10){
+					else if(r == 10) {
 						System.out.println("I/O");
 					} 
 				}
-				else if(r==10){
+				else if(r==10) {
 					System.out.println("FLow Control");
 				}
 
@@ -47,69 +47,30 @@ public class WhiteSpace {
 	// 9 -> tab
 	// 10 -> line feed
 
-	public int convert(FileInputStream fis){
+	public int convert(FileInputStream fis) throws IOException {
 		int r;
 		int i=0;
 		boolean positive = true;
 		StringBuilder sb = new StringBuilder(); 
 		r=fis.read();
 
-		if( r == 9){
+		if(r == 9) {
 			// negative
 			positive = false;
 		}
 
-		while((r = fis.read())!= 10){
-			if(r == 32){
+		while((r = fis.read())!= 10) {
+			if(r == 32) {
 				sb.append("0");
 			}
-			else if(r==9){
+			else if(r==9) {
 				sb.append("1");
 			}
 		}
 		int temp = Integer.parseInt(sb.toString(),2);
-		if(!positive){
+		if(!positive) {
 			temp*= -1;
 		}
 		return temp;
-
-
-	}
-
-	public static void main(String[] args) {
-		WhiteSpace obj = new WhiteSpace();
-		obj.parsing();
-		// Stack_manipulation st = new Stack_manipulation();
-		// st.push(1);
-		// st.push(2);
-		// st.push(3);
-		// st.push(4);
-		// st.push(5);					//1,2,3,4,5 -top->5
-		// System.out.println(st.s);
-		
-		// st.duplicateTop();			//1,2,3,4,5,5
-		// System.out.println(st.s);
-		
-		// st.copy(1);					//1,2,3,4,5,5,2
-		// System.out.println(st.s);
-		
-		// st.swap();					//1,2,3,4,5,2,5
-		// System.out.println(st.s);
-
-		// System.out.println('\n');
-		// Arithmetic a = new Arithmetic();
-		// a.s.push(1);
-		// a.s.push(2);
-		// a.s.push(3);
-		// a.s.push(4);
-		// a.s.push(5);
-		// a.add();
-		// System.out.println(a.s);
-		// a.subtract();
-		// System.out.println(a.s);
-		// a.multiply();
-		// System.out.println(a.s);
-		// a.divide();
-		// System.out.println(a.s);
 	}
 }
