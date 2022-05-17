a5fb8d657f828022a0f95ed1c7f9f29580e3baa2
Amitash M S <amitu895@gmail.com>
2022-05-17 06:55:59 +0530

    Clean up of code

--- WhiteSpace.java
+++ WhiteSpace.java
@@ -4,35 +4,40 @@ import java.io.FileReader;
 import java.io.*;  
 import java.io.IOException;  
 
-
 public class WhiteSpace {
 	public void parsing() {
 		try {
-			File f = new File("./sample.ws");
+			File f = new File("./sample.ws"); // File containing whitespace source code
 			FileInputStream fis = new FileInputStream(f);
-			int r = 0;
+			int r = 0; // Variable to store every character read from the file (one at a time)
+
 			while((r = fis.read()) != -1) {
 				// System.out.print((char)r);
-				if(r== 32) { 
+				if(r == 32) { 
 					System.out.println("Stack Manipulation");
+					// Code to perform stack manipulation operations
 				}
 
-				else if(r==9) {
-					r=fis.read();
+				else if(r == 9) {
+					r = fis.read();
 					if(r == 32) {
 						System.out.println("Arithmetic");
+						// Code to perform arithmetic operations
 					} 
 					else if(r == 9) {
 						System.out.println("Heap Access");
+						// Code to perform heap operations
 					} 
 					else if(r == 10) {
 						System.out.println("I/O");
+						// Code to perform i/o operations
 					} 
 				}
+
 				else if(r==10) {
 					System.out.println("FLow Control");
+					// Code to perform flow control operations
 				}
-
 			}
 		}
 		catch (FileNotFoundException e) {
@@ -47,30 +52,33 @@ public class WhiteSpace {
 	// 9 -> tab
 	// 10 -> line feed
 
+	// Function to convert a number in whitespace to integer
 	public int convert(FileInputStream fis) throws IOException {
-		int r;
-		int i=0;
-		boolean positive = true;
+		int r, i = 0, res;
+		boolean positive = true; // Assuming the number to be positive by default
 		StringBuilder sb = new StringBuilder(); 
-		r=fis.read();
+		r = fis.read();
 
 		if(r == 9) {
 			// negative
 			positive = false;
 		}
 
+		// Read each character and store it as a binary string
 		while((r = fis.read())!= 10) {
 			if(r == 32) {
 				sb.append("0");
 			}
-			else if(r==9) {
+			else if(r == 9) {
 				sb.append("1");
 			}
 		}
-		int temp = Integer.parseInt(sb.toString(),2);
+
+		// Convert the binary string to a final integer
+		res = Integer.parseInt(sb.toString(), 2);
 		if(!positive) {
-			temp*= -1;
+			res *= -1;
 		}
-		return temp;
+		return res;
 	}
 }
